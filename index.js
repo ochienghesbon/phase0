49460e15d950189fa41961134538ced228a6573e
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-05 01:52:34 +0300

    adds js content

---
+++ index.js
@@ -0,0 +1,15 @@
+let image=document.getElementById("img")
+let show=document.getElementById("hide")
+let hide=document.getElementById("show")
+
+hide.onclick=function(){
+    image.style.display='none'
+}
+
+show.onclick=function(){
+    image.style.display='block'
+}
+alert(hello);
+
+
+
